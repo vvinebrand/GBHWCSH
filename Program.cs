f43b0d34ea0642445754aa5494d01fc5163f954f
Viktoriia Nekrasova <vvinebrand@gmail.com>
2023-01-26 14:34:43 +0300

    Скелет программы

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
